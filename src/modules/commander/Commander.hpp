4a52c00e2a652c265598bad022a48226250a7acd
David Jablonski <dayjaby@gmail.com>
2020-05-06 09:56:40 +0200

    moved do_set_servo to commander

--- src/modules/commander/Commander.hpp
+++ src/modules/commander/Commander.hpp
@@ -47,6 +47,7 @@
 // publications
 #include <uORB/Publication.hpp>
 #include <uORB/topics/actuator_armed.h>
+#include <uORB/topics/actuator_controls.h>
 #include <uORB/topics/home_position.h>
 #include <uORB/topics/test_motor.h>
 #include <uORB/topics/vehicle_command_ack.h>
@@ -421,6 +422,7 @@ private:
 
 	// Publications
 	uORB::Publication<actuator_armed_s>			_armed_pub{ORB_ID(actuator_armed)};
+	uORB::Publication<actuator_controls_s>			_actuator_controls_2_pub{ORB_ID(actuator_controls_2)};
 	uORB::Publication<commander_state_s>			_commander_state_pub{ORB_ID(commander_state)};
 	uORB::Publication<test_motor_s>				_test_motor_pub{ORB_ID(test_motor)};
 	uORB::Publication<vehicle_control_mode_s>		_control_mode_pub{ORB_ID(vehicle_control_mode)};
